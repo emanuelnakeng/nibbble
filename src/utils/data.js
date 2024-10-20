9441b8a167413ae08aca3ab7d35f44304622e3fa
emanuelnakeng <emanuelnakeng@gmail.com>
2024-10-20 11:59:09 +0200

    Added breakfast image uri

--- src/utils/data.js
+++ src/utils/data.js
@@ -2,7 +2,7 @@ const categories = [
 	{
 		name: 'Breakfast',
 		imageURI:
-			'https://images.unsplash.com/photo-1617548059706-d8a95dd91fe7?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+			'https://images.unsplash.com/photo-1502747220144-846486e80891?q=80&w=2012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
 	},
 	{
 		name: 'Smoothies',
